b2f3f50cbc7e0d5bfade97c7cb1371b92fb92fe8
Swaraj Gavali <swarajgavali.ai@gmail.com>
2026-05-02 20:15:17 +0530

    Day 1: Add dataset setup

---
+++ 01_SQL_Basics/01_SQL_Basics/day1/01_SQL_Basics/day1/setup.sql
@@ -0,0 +1,20 @@
+CREATE TABLE employees (
+    emp_id INT,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    age INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO employees VALUES
+(1, 'Amit', 'IT', 60000, 28, 'Mumbai'),
+(2, 'Neha', 'HR', 45000, 32, 'Pune'),
+(3, 'Raj', 'Finance', 70000, 35, 'Mumbai'),
+(4, 'Simran', 'IT', 52000, 26, 'Delhi'),
+(5, 'Karan', 'HR', 48000, 29, 'Bangalore'),
+(6, 'Priya', 'Finance', 65000, 31, 'Mumbai'),
+(7, 'Ankit', 'IT', 55000, 27, 'Pune'),
+(8, 'Sneha', 'Marketing', 40000, 24, 'Delhi'),
+(9, 'Vikas', 'Finance', 72000, 34, 'Bangalore'),
+(10, 'Riya', 'HR', 47000, 30, 'Mumbai');
